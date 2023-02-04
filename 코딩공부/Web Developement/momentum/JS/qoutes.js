d09ac4b4586404ed782c91f24a5fc7ff1d0f4eb9
neou11 <nambeen08@naver.com>
2023-02-04 16:48:19 +0900

    adding random qoutes with authors

---
+++ 코딩공부/Web Developement/momentum/JS/qoutes.js
@@ -0,0 +1,56 @@
+const qoutes = [
+  {
+    qoute: "A good beginning makes a good ending.",
+    author: "English Sentence",
+  },
+  {
+    qoute: "For I can raise no money by vile means.",
+    author: "William Shakespeare",
+  },
+  {
+    qoute: "You can't blame gravity for falling in love.",
+    author: "Albert Einstein",
+  },
+  {
+    qoute: "Apparently three is nothing that cannot happen today.",
+    author: "Mark Twain",
+  },
+  {
+    qoute: "Reality is merely an illusion, albeit a very persistent one.",
+    author: "Albert Einstein",
+  },
+  {
+    qoute: "Truth is the most valuable thing we have. Let us economize it.",
+    author: "Mark Twain",
+  },
+  {
+    qoute:
+      "If tou can't explain it simply, you don't understand it well enough.",
+    author: "Albert Einstein",
+  },
+  {
+    qoute: "Never do anything against conscience even if the state demands it.",
+    author: "Albertinstein",
+  },
+  {
+    qoute: "A clever man commits no minor blunders",
+    author: "Johann Wolfgang Von Goethe",
+  },
+  {
+    qoute: "You don't know what you've got until you've lost it.",
+    author: "proverb",
+  },
+];
+
+const qoute = document.querySelector("#qoute span:first-child");
+const author = document.querySelector("#qoute span:last-child");
+
+const TodaysQoute = qoutes[Math.floor(Math.random() * qoutes.length)];
+console.log(TodaysQoute);
+// TodaysQoute.qoute = q;
+// TodaysQoute.author = a;
+
+qoute.innnerText = TodaysQoute.qoute;
+author.innnerText = TodaysQoute.author;
+
+console.log(qoute.innnerText);
