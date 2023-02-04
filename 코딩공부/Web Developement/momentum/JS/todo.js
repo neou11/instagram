8d045b0338bd97331995a72a1f341eb23412ccc0
neou11 <nambeen08@naver.com>
2023-02-04 23:00:52 +0900

    adding todolist with buttons

---
+++ 코딩공부/Web Developement/momentum/JS/todo.js
@@ -0,0 +1,29 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todo-list");
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  span.innerText = newTodo;
+  todoList.appendChild(li);
+}
+
+function handleTodosubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  paintTodo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleTodosubmit);
