5146475b9a7ffe64aeeeea71de38f0c964d66868
neou11 <nambeen08@naver.com>
2023-02-05 17:57:51 +0900

    completing deleting function on todos

--- 코딩공부/Web Developement/momentum/JS/todo.js
+++ 코딩공부/Web Developement/momentum/JS/todo.js
@@ -1,29 +1,53 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
+
+let todos = [];
+
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
 
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+  saveTodos();
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteTodo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
   todoList.appendChild(li);
+  span.innerText = newTodo.text;
 }
 
 function handleTodosubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
-  paintTodo(newTodo);
+  const newTodoObj = {
+    id: Date.now(),
+    text: newTodo,
+  };
+  todos.push(newTodoObj);
+  paintTodo(newTodoObj);
+  saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodosubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
